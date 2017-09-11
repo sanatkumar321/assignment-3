efffd91057aaba96fa20499c286f7e7df2b0b08d
sanatkumar321 <31204486+sanatkumar321@users.noreply.github.com>
2017-09-11 16:59:07 +0530

    Add files via upload

---
+++ q31.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int count = 0;
+
+    cout<<"Enter an integer: ";
+    cin>>n;
+
+    while(n != 0)
+    {
+       
+        n /= 10;
+        ++count;
+    }
+
+    cout<< count;
+}
